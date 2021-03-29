e166a944578098a9affa8940eb84be9585f694bd
Riggs Markham <riggsmarkham@gmail.com>
2021-03-28 20:11:00 -0500

    added 400

--- app.py
+++ app.py
@@ -25,4 +25,8 @@ def upload_file():
         )
 @app.errorhandler(404)
 def page_not_found(error):
-   return render_template('404.html', title = '404'), 404
+   return render_template('404.html')
+
+@app.errorhandler(400)
+def page_not_found(error):
+   return render_template('400.html')
