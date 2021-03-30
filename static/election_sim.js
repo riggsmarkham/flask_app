4c7fdeb467b956772a280f4199cc985f055d0814
Riggs Markham <riggsmarkham@gmail.com>
2021-03-30 14:40:50 -0500

    significantly cleaned up reveal function execution

--- static/election_sim.js
+++ static/election_sim.js
@@ -1,32 +1,41 @@
 var selection = [];
-function toggle_display(class_name, row_name, clicked_el){
+button_list = document.getElementsByTagName('button');
+for(el of button_list){
+    if(el.id.slice(0,1) == "1"){
+        el.style.display = 'inline';
+    }
+}
+function reveal(el_list, clicked_el){
+    button_list = document.getElementsByTagName('button');
     if(!clicked_el.classList.contains('clicked')){
-        clicked_el.classList.add('clicked')
-        selection.push(class_name);
-        class_items = document.getElementsByClassName(class_name);
-        for(el of class_items){
-            el.style.display = 'inline'
-        };
-        row_items = document.getElementsByClassName(row_name);
-        for(el of row_items){
-            el.disabled = true;
-        };
-        clicked_el.disabled = false;
-    }else{
+        clicked_el.classList.add('clicked');
+        selection.push(clicked_el.id.slice(2));
+        for(el of el_list){
+            document.getElementById(el).style.display = 'inline';
+        }
+        for(el of button_list){
+            if((el.id.slice(0,1) == clicked_el.id.slice(0,1)) & (el.id != clicked_el.id)){
+                el.disabled = true;
+            }
+        }
+    } else {
         clicked_el.classList.remove('clicked')
-        selection.pop();
-        row_items = document.getElementsByClassName(row_name);
-        for(el of row_items){
-            el.disabled = false;
+        num = parseInt(clicked_el.id.slice(0,1));
+        selection = selection.slice(0,num - 1);
+        for(el of button_list){
+            if(el.id.slice(0,1) == clicked_el.id.slice(0,1)){
+                el.disabled = false;
+            }
         };
-        if(document.getElementsByClassName(class_name)[0]){
-            next_row_items = document.getElementsByClassName(document.getElementsByClassName(class_name)[0].classList[0])
-            for(el of next_row_items){
-                el.style.display = 'none'
-            };
+        for(el of button_list){
+            if(parseInt(el.id.slice(0,1)) > num){
+                el.classList.remove('clicked');
+                el.disabled = false;
+                el.style.display = 'none';
+            }
         }
     }
-    console.log(selection)
+    console.log(selection);
     //document.getElementById('displaything').innerHTML = selection;
 }
 
@@ -47,33 +56,33 @@ function submit(){
         if (selection[3] == "file"){
             reveal_file_submission();
         } else {
-            reveal_appr_form()
+            reveal_appr_form();
         }
     } else {
         if (selection[3] == "file"){
-            reveal_file_submission()
+            reveal_file_submission();
         } else {
-            reveal_pair_form()
+            reveal_pair_form();
         }
     }
 }
 
 function reveal_file_submission(){
-    document.getElementById('sub_form').style.visibility = 'visible'
+    document.getElementById('sub_form').style.visibility = 'visible';
 }
 
 function reveal_fptp_form(){
-    document.getElementById('fptp_form').style.visibility = 'visible'
+    document.getElementById('fptp_form').style.visibility = 'visible';
 }
 
 function reveal_pref_form(){
-    document.getElementById('pref_form').style.visibility = 'visible'
+    document.getElementById('pref_form').style.visibility = 'visible';
 }
 
 function reveal_appr_form(){
-    document.getElementById('appr_form').style.visibility = 'visible'
+    document.getElementById('appr_form').style.visibility = 'visible';
 }
 
 function reveal_pair_form(){
-    document.getElementById('pair_form').style.visibility = 'visible'
+    document.getElementById('pair_form').style.visibility = 'visible';
 }
