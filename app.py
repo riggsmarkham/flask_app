ebb997bebb2e11e3ff265fb220b39afc805d84db
Riggs Markham <riggsmarkham@gmail.com>
2021-04-09 10:59:46 -0500

    did a bit

--- app.py
+++ app.py
@@ -8,44 +8,35 @@ os.makedirs(os.path.join(app.instance_path, 'uploads'), exist_ok=True)
 
 @app.route("/")
 def home():
-    return render_template(
-        "homepage.html"
-    )
+    return render_template("homepage.html")
+
 @app.route("/election_sim", methods = ['GET'])
 def election_app():
-    return render_template(
-        "election_sim.html"
-    )
+    return render_template("election_sim.html")
+
 @app.route('/election_sim/submission', methods = ['POST'])
 def upload_file():
    if request.method == 'POST':
         f = request.files['file']
-        f.save(os.path.join(app.instance_path, 'uploads', secure_filename(f.filename)))
-        sim.doAllSystems('Simulation', 'instance/uploads/' + f.filename, sim.NUM, False)
+        fname = secure_filename(f.filename)
+        f.save(os.path.join(app.instance_path, 'uploads', fname))
+        sim.doAllSystems('Simulation', 'instance/uploads/' + fname, sim.NUM, False)
+        os.remove(os.path.join(app.instance_path, 'uploads', fname))
         return render_template(
-            "submission.html", filename = f.filename, type = "file"
+            "submission.html", filename = fname
         )
-
-# @app.route('/election_sim/result', methods = ['POST'])
-# def run_sim():
-#     if request.method == 'POST':
-#         filename = request.form['filename']
-#         doAllSystems('Simulation', 'instance/uploads/' + filename, NUM, False)
-#         return render_template(
-#             "result.html"
-#         )
 
 @app.errorhandler(404)
 def page_not_found(error):
-   return render_template('errors/404.html')
+   return render_template('errors/404.html'),404
 
 @app.errorhandler(400)
 def bad_request(error):
-   return render_template('errors/400.html')
+   return render_template('errors/400.html'),400
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    return render_template('errors/500.html')
+    return render_template('errors/500.html'),500
 
 if __name__ == "__main__" :
     app.run()
