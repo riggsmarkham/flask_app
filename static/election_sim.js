b99961fe295d5cd121c20a7240c6c4f6f7f24a06
Riggs Markham <riggsmarkham@gmail.com>
2021-04-12 21:11:08 -0500

    table builder

--- static/election_sim.js
+++ static/election_sim.js
@@ -6,8 +6,7 @@ for(el of total_button_list){
         button_list.push(el);
     }
 }
-console.log(total_button_list.length);
-console.log(button_list.length);
+
 for(el of button_list){
     if(el.id.slice(0,1) == "1"){
         el.style.display = 'inline';
@@ -83,8 +82,35 @@ function submit(el){
 }
 
 function populate_fptp_form(){
-    val = document.getElementById("fptp_num").value;
-    console.log(val);
+    var val = document.getElementById("fptp_num").value;
+    addTable(val,"Candidate Name", "Percent Support", "fptp_input", "fptp_input_sub")
     document.getElementById("fptp_input").style.display='inline';
     document.getElementById("fptp_setup").style.display='none';
+}
+
+function populate_appr_form(){
+    var val = document.getElementById("appr_num").value;
+    addTable(val,"Candidate Name", "Percent Approval", "appr_input", "appr_input_sub")
+    document.getElementById("appr_input").style.display='inline';
+    document.getElementById("appr_setup").style.display='none';
+}
+
+function addTable(val, col_1, col_2, parent_id, child_id){
+    const newTable = document.createElement("table");
+    num = parseInt(val);
+    var r = newTable.insertRow();
+    var c = r.insertCell();
+    c.innerHTML = col_1;
+    c = r.insertCell();
+    c.innerHTML = col_2;
+    for (i = 1; i < num + 1; i++){
+        r = newTable.insertRow();
+        c = r.insertCell();
+        c.innerHTML = "<input id=txt" + i.toString() + "-1 type = 'text'></input>";
+        c = r.insertCell();
+        c.innerHTML = "<input id=txt" + i.toString() + "-2 type = 'text'></input>";
+    }
+    var parent = document.getElementById(parent_id);
+    var child = document.getElementById(child_id);
+    parent.insertBefore(newTable, child);
 }
